bfc535c980097146b7a46dbb1ec43273de9e9fee
angeloboss1 <abossini@drew.edu>
2019-10-25 10:07:18 -0400

    Update login.php

--- login.php
+++ login.php
@@ -58,34 +58,33 @@
 
 <br>
 <?php
-	session_start();
-	$username = $_POST['username'];
-	$password = $_POST['psw'];
-	$password = base64_encode($password);
-	$_SESSION["username"] = $username;
+session_start();
+$username = $_POST['username'];
+$password = $_POST['psw'];
+$password = base64_encode($password);
 
 
-	$username = stripcslashes($username);
-	$password = stripcslashes($password);
+
+$username = stripcslashes($username);
+$password = stripcslashes($password);
 
 
-	$conn = new mysqli("localhost", "catering_admin", "Drew2019@", "catering_logins");
-	$_SESSION["connection"] = $conn;
-	if ($conn->connect_error) {
-	die("Connection failed: " . $conn->connect_error);
-	}
-	$sql= "SELECT * FROM users WHERE username = '$username' and password = '$password'";
-	$result = $conn->query($sql);
-	$row = $result->fetch_assoc();
-	if($result->num_rows > 0){
+$conn = new mysqli("localhost", "catering_admin", "Drew2019@", "catering_logins");
+$_SESSION["connection"] = $conn;
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+}
+$sql= "SELECT * FROM users WHERE username = '$username' and password = '$password'";
+$result = $conn->query($sql);
+$row = $result->fetch_assoc();
+if($result->num_rows > 0){
+$_SESSION["username"] = $username;
 ?>
     <script type="text/javascript">
 window.location.href = 'userhome.php';
 </script>
 <?php
-session_start();
-$_SESSION["username"] = $username;
-exit;
+exit();
 	}
 	else{
 	echo "<div class='alert alert-danger' role='alert'>This is a danger alert—check it out!</div>";
