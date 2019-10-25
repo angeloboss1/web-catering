b8d2093a8d88b06546861b86c697db535adc8d4e
angeloboss1 <abossini@drew.edu>
2019-10-25 00:20:10 -0400

    Update userhome.php

--- userhome.php
+++ userhome.php
@@ -75,56 +75,7 @@ session_start();
   <h1>Welcome to SDA Catering  </h1><br><br>
   <div class="row">
     <div class="col-sm-4" >
-	<?php
-	  $conn = new mysqli("localhost", "catering_admin", "Drew2019@", "catering_logins");
-		$_SESSION["connection"] = $conn;
-		$username = $_SESSION["username"] ;
-		if ($conn->connect_error) {
-			die("Connection failed: " . $conn->connect_error);
-		}
-		$outofstockQuery= "SELECT * FROM inventory WHERE username = '$username' AND Quantity = '0'";
-		$result = $conn->query($outofstockQuery);
-		echo "<h5>You have " . $result->num_rows . " items out of stock</h5>";
-		?>
-      <a href="outofstock.php"><img src="/image/OutOfStock.jpg" class="img-responsive" style="width:100%; height: 60%;" alt="Image"></a>
-      <hr>
-    </div>
 
-    <div class="col-sm-4">
-      	<?php
-		$conn = new mysqli("localhost", "angelobo_a", "angeloboss12", "angelobo_logins");
-		$_SESSION["connection"] = $conn;
-		//$username = $_SESSION["username"] ;
-		if ($conn->connect_error) {
-			die("Connection failed: " . $conn->connect_error);
-		}
-		$lowstockQuery= "SELECT * FROM inventory WHERE username = '$username' AND Quantity <= lowstockfrom AND Quantity > '0' ";
-		$result = $conn->query($lowstockQuery);
-		echo "<h5>You have " . $result->num_rows . " items low stock</h5>";
-		?>
-      <a href="lowstock.php"><img src="/image/LowStock.jpg" class="img-responsive" style="width:100%; max-height: 60%;" alt="Image"></a>
-      <hr>
-      <br><br>
-    </div>
-
-
-    <div class="col-sm-4">
-      <?php
-						$conn = new mysqli("localhost", "angelobo_a", "angeloboss12", "angelobo_logins");
-	$_SESSION["connection"] = $conn;
-
-	if ($conn->connect_error) {
-	die("Connection failed: " . $conn->connect_error);
-	}
-	$sql= "SELECT sku, Product_name, Product_desc, Quantity FROM inventory WHERE username = '$username'";
-	$result = $conn->query($sql);
-	echo "<h5>You have " . $result->num_rows . " items in your inventory</h5>";
-	?>
-      <a href="yourinventory.php"><img src="/image/inventory.png"  class="img-responsive" style="width:100%; height: 60%;" alt="Image"></a>
-      <hr>
-    </div>
-  </div>
-</div>
 
 <footer class="page-footer font-small bg-dark pt-4" style="bottom: 0; width: 100%; text-align: center;">
   <!-- Copyright -->
