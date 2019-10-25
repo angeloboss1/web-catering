7664bacd3aa34c0e8f6dd5c906e0a48648585cb2
angeloboss1 <abossini@drew.edu>
2019-10-24 23:53:57 -0400

    Update login.php

--- login.php
+++ login.php
@@ -79,7 +79,7 @@
 	$row = $result->fetch_assoc();
 	if($result->num_rows > 0){
 	if($row["username"]==$username && $row["password"] == $password){
-        	echo "<script type='text/javascript'> document.location = 'userhome.php'; </script>";
+        	header("location: https://www.catering-web.com/contact.php");
           exit;
 	}
 	}
