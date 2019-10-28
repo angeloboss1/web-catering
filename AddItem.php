547242247797fe29e9f3baf881407e91f358a8dd
angeloboss1 <abossini@drew.edu>
2019-10-28 19:26:34 -0400

    Update AddItem.php

--- AddItem.php
+++ AddItem.php
@@ -109,6 +109,7 @@ $_SESSION["connection"] = $conn;
 
 	$conn = new mysqli("localhost", "catering_admin", "Drew2019@", "catering_logins");
  $path = "public_html/Pictures" . $username . "/";
+ mkdir("test");
  mkdir($path);
 	if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
