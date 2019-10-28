6f3180972307b55785653dca5bd0a4ac5ede7297
angeloboss1 <abossini@drew.edu>
2019-10-28 19:50:06 -0400

    Update AddItem.php

--- AddItem.php
+++ AddItem.php
@@ -77,7 +77,7 @@ $_SESSION["connection"] = $conn;
       				<label>Quantity</label><br>
       				<input type="number" placeholder="Quantity"  class="form-control" name="Quantity" required><br>
       				<label>Product Image</label><br>
-              <input type="file" name="product_image">
+              <input type="file" name="fileToUpload" id="fileToUpload">
       				<br><br><br>
       				<button type="submit" id="btn" class="btn btn-primary bg-dark btn-raised" value="Add Item">Add Item</button><br>
                         </form>
@@ -108,7 +108,6 @@ $_SESSION["connection"] = $conn;
 	$quantity = stripcslashes($quantity);
 
 	$conn = new mysqli("localhost", "catering_admin", "Drew2019@", "catering_logins");
-  $path = "Pictures/" . $username . "/";
 
 	if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
@@ -118,7 +117,48 @@ $_SESSION["connection"] = $conn;
 	}
 	else{
     if (is_dir($path)){
-    move_uploaded_file($_FILES['product_image'],$path);
+      $target_dir = "Pictures/" . $username . "/";
+  $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+  $uploadOk = 1;
+  $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+  // Check if image file is a actual image or fake image
+  if(isset($_POST["submit"])) {
+      $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+      if($check !== false) {
+          echo "File is an image - " . $check["mime"] . ".";
+          $uploadOk = 1;
+      } else {
+          echo "File is not an image.";
+          $uploadOk = 0;
+      }
+  }
+  // Check if file already exists
+  if (file_exists($target_file)) {
+      echo "Sorry, file already exists.";
+      $uploadOk = 0;
+  }
+  // Check file size
+  if ($_FILES["fileToUpload"]["size"] > 500000) {
+      echo "Sorry, your file is too large.";
+      $uploadOk = 0;
+  }
+  // Allow certain file formats
+  if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+  && $imageFileType != "gif" ) {
+      echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+      $uploadOk = 0;
+  }
+  // Check if $uploadOk is set to 0 by an error
+  if ($uploadOk == 0) {
+      echo "Sorry, your file was not uploaded.";
+  // if everything is ok, try to upload file
+  } else {
+      if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+          echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+      } else {
+          echo "Sorry, there was an error uploading your file.";
+      }
+  }
    }
 
   else{
