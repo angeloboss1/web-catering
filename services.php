d53b5df6a61894a901ba253f27a52283f67cc838
angeloboss1 <abossini@drew.edu>
2019-10-28 10:38:01 -0400

    Update services.php

--- services.php
+++ services.php
@@ -1,14 +1,13 @@
 <?php
-/*$conn = new mysqli("localhost", "catering_admin", "Drew2019@", "catering_logins");
+$conn = new mysqli("localhost", "catering_admin", "Drew2019@", "catering_logins");
 if ($conn->connect_error) {
 die("Connection failed: " . $conn->connect_error);
 }
-$sql= "SELECT * FROM catalogue";
-$result = mysql_query($sql);
-while ($row = mysql_fetch_array($result))
+$sql= "SELECT * FROM users WHERE username = '$username' and password = '$password'";
+$result = $conn->query($sql);
+while ($row = $result->fetch_assoc())
 {
  echo $row['id_product'];
 }
-*/
 echo 'hello';
 ?>
