9eb004307bf227f3d901f2008822de14a5596c61
angeloboss1 <abossini@drew.edu>
2019-10-28 20:57:23 -0400

    Update AddItem.php

--- AddItem.php
+++ AddItem.php
@@ -109,8 +109,6 @@ $_SESSION["connection"] = $conn;
 
 	$conn = new mysqli("localhost", "catering_admin", "Drew2019@", "catering_logins");
   $path = "Pictures/" . $username . "/";
-$filename= $_FILES["fileToUpload"]['tmp_name'];
-$new_file_name = strtolower($_FILES['photo']['tmp_name']);
 	if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
 	}
@@ -119,9 +117,48 @@ $new_file_name = strtolower($_FILES['photo']['tmp_name']);
 	}
 	else{
     if (is_dir($path)){
-      $target_dir = "Pictures/" . $username . "/" . $new_file_name ;
-echo "hello" . $_FILES["fileToUpload"]['name'];
-move_uploaded_file($filename, $target_dir);
+      $target_dir = $path;
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
 
   }
 
