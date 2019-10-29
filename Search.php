d5457e06b1112ec548ce896daec7224deac10e3d
angeloboss1 <abossini@drew.edu>
2019-10-29 11:29:01 -0400

    Update Search.php

--- Search.php
+++ Search.php
@@ -1,5 +1,5 @@
 <?php
- 
+
 session_start();
 $username = $_SESSION["username"] ;
  if (!isset($_SESSION["username"]))
@@ -29,7 +29,7 @@ $(function () {
         evenRow: { 'background-color': '#fff' },
         searchType: 'fuzzy'
     });
-    
+
 });
 </script>
 </head>
@@ -49,14 +49,14 @@ $(function () {
 			<a class="dropdown-item" href="yourinventory.php" style="color:gray;">All Inventory</a>
 			<a class="dropdown-item" href="outofstock.php" style="color:gray;">Out of Stock</a>
 			<a class="dropdown-item" href="lowstock.php" style="color:gray;">Low Stock</a>
-			
-		</div>			
+
+		</div>
 		</div>
                 <a href="AddItem.php" class="nav-item nav-link">Add Item</a>
                 <a href="Search.php" class="nav-item nav-link active">Search Item</a>
-            </div>	
+            </div>
            <div id="navbarCollapse" class="collapse navbar-collapse justify-content-start">
-		
+
 		<ul class="nav navbar navbar-expand-md navbar-dark bg-dark ml-auto">
 			<li class="nav-item">
 			<a class="nav-link" href="#" style="color: grey;"><i class="fa fa-user-o" ></i><?php session_start(); echo " ". $_SESSION["username"]?></a>
@@ -68,19 +68,19 @@ $(function () {
 		</ul>
 	</div>
         </div>
-    </div>    
-        
+    </div>
+
 </nav>
 <div class="container">
     <div class="row">
-        
+
         <div class="col-lg-6">
             <h3>Search in your Inventory</h3>
             <input type="search" id="search" value="" class="form-control" placeholder="Search in your inventory (Fuzzy)">
             <br><br>
         </div>
    <br><br>
-    
+
   <div class="col-lg-6">
   <div class="form-group form-inline">
 <form action="Search.php" method="POST" >
@@ -101,46 +101,46 @@ session_start();
 	$sku = $_POST["sku"];
 	$username = $_SESSION["username"] ;
 	$conn1 = new mysqli("localhost", "angelobo_a", "angeloboss12", "angelobo_logins");
-	if ($conn1->connect_error) { 
+	if ($conn1->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
 	}
 	$query= "DELETE FROM inventory WHERE username = '$username' AND sku = '$sku'";
 	if($sku===NULL){
-	
+
 	}
 	else{
-	
-	if($conn1->query($query) === TRUE){ 
+
+	if($conn1->query($query) === TRUE){
     		echo "<div class='alert alert-success' role='alert'> You successfully delete the item.</div>";
-	}  
-	else{ 
-    	echo "ERROR: Could not able to execute $sql. "  . mysqli_error($conn1); 
+	}
+	else{
+    	echo "ERROR: Could not able to execute $sql. "  . mysqli_error($conn1);
     	echo "<div class='alert alert-danger' role='alert'> You already entered an item with the same SKU.</div>";
 	} }
-    		
+
 ?>
 
-    
+
         <div class="col-lg-12">
-       
- 
+
+
             <?php
-						$conn = new mysqli("localhost", "angelobo_a", "angeloboss12", "angelobo_logins");
+						$conn = new mysqli("localhost", "catering_admin", "Drew2019@", "catering_logins");
 	$_SESSION["connection"] = $conn;
-	
-	if ($conn->connect_error) { 
+
+	if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
 	}
-	$sql= "SELECT sku, Product_name, Product_desc, Quantity FROM inventory WHERE username = '$username'";
+	$sql= "SELECT id_product, Product_name, Product_desc, Quantity FROM catalogue";
 	$result = $conn->query($sql);
 	if ($result->num_rows > 0) {
 	?>
 </div>
 
-				
+
 					<table id="table"  class="table table-sm table-striped" style="width:96%; text-align:center;" align="center">
   					<thead class="thead-dark">
-					      <th>SKU</th>
+					      <th>Product Id</th>
 					      <th>Product Name</th>
 					      <th>Product Description</th>
 					      <th>Quantity</th>
@@ -149,17 +149,17 @@ session_start();
 					  <tbody>
 <?php
     while($row = $result->fetch_assoc()) {
-    	
-        echo "<tr> <td>" . $row["sku"] . "</td><td>". $row["Product_name"]."</td>" . "<td>" . $row["Product_desc"] . "</td>" . "<td>" . $row["Quantity"] . "</td></tr>";
+
+        echo "<tr> <td>" . $row["id_product"] . "</td><td>". $row["Product_name"]."</td>" . "<td>" . $row["Product_desc"] . "</td>" . "<td>" . $row["Quantity"] . "</td></tr>";
     }
 } else {
     echo "0 results";
 }
-	
+
 	mysqli_close($conn);
 	?>
             <hr>
-            
+
 
 
 <script src="//rawgithub.com/stidges/jquery-searchable/master/dist/jquery.searchable-1.0.0.min.js"></script>
