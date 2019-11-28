35198fa00e6873fc0c0baedf40da4908b8f123d8
angeloboss1 <abossini@drew.edu>
2019-11-28 15:09:40 -0500

    Update navigation.php

--- navigation.php
+++ navigation.php
@@ -1,38 +1,55 @@
 <!-- navbar -->
-<div class="navbar navbar-default navbar-static-top" role="navigation">
-	<div class="container">
+<div class="container-fluid">
+		<a href="#" class="navbar-brand mr-3">SDA Catering</a>
+		<button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+				<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarCollapse">
+				<div class="navbar-nav">
+						<a href="index.html" class="nav-item nav-link active">Home</a>
+						<a href="products.php" class="nav-item nav-link">Products</a>
+						<a href="contact.php" class="nav-item nav-link">Contact</a>
+				</div>
+				<div id="navbarCollapse" class="collapse navbar-collapse justify-content-start">
 
-		<div class="navbar-header">
-			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-			<span class="sr-only">Toggle navigation</span>
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-			</button>
-			<a class="navbar-brand" href="products.php">XYZ Webstore</a>
+<ul class="nav navbar navbar-expand-md navbar-dark bg-dark ml-auto">
+	<li <?php session_start();
+	echo $page_title=="Cart" ? "class='active'" : ""; ?> >
+		<a href="cart.php">
+			<?php
+			// count products in cart
+			$cart_count=count($_SESSION['cart']);
+			?>
+			Cart <span class="badge" id="comparison-count"><?php echo $cart_count; ?></span>
+		</a>
+	</li>
+	<li class="nav-item dropdown">
+		<a data-toggle="dropdown" class="nav-link dropdown-toggle" href="#" style="color: grey;"><i class="fa fa-user-o" ></i> Login</a>
+		<ul class="dropdown-menu">
+
+			<li>
+										<form class="form-inline login-form" action="login.php" method="POST">
+												<div class="input-group">
+														<input type="text" class="form-control" name="username" style="width: 90%; margin-left:5%; margin-right:5%;" placeholder="Username" required>
+												</div>
+												<div class="input-group" >
+														<input type="password" class="form-control" name="psw" style="width: 90%; margin-left:5%; margin-top:5%; margin-right:5%;" placeholder="Password" required>
+												</div>
+												<button type="submit" class="btn bg-dark btn-primary" style="margin-left:10px; margin-top: 10px; margin-right: 10px;">Login</button>
+
+										</form>
+			</li>
+
+
+		</ul>
+
+	</li>
+
+	 <!-- <li class="nav-item">
+	<a href="register.php" style="color: grey;"><i class="fa fa-user-o"></i> Register</a>
+</li> -->
+</ul>
+</div>
 		</div>
-
-		<div class="navbar-collapse collapse">
-			<ul class="nav navbar-nav">
-
-				<!-- highlight if $page_title has 'Products' word. -->
-				<li <?php echo $page_title=="Products" ? "class='active'" : ""; ?>>
-					<a href="products.php" class="dropdown-toggle">Products</a>
-				</li>
-
-				<li <?php echo $page_title=="Cart" ? "class='active'" : ""; ?> >
-					<a href="cart.php">
-						<?php
-						// count products in cart
-						$cart_count=count($_SESSION['cart']);
-						?>
-						Cart <span class="badge" id="comparison-count"><?php echo $cart_count; ?></span>
-					</a>
-				</li>
-			</ul>
-
-		</div><!--/.nav-collapse -->
-
-	</div>
 </div>
 <!-- /navbar -->
