7a688a6674b8d7c61d6a112448fd165b5de14562
angeloboss1 <abossini@drew.edu>
2019-11-28 16:01:12 -0500

    Update cart.php

--- cart.php
+++ cart.php
@@ -25,7 +25,7 @@ include 'layout_head.php';
 
 $action = isset($_GET['action']) ? $_GET['action'] : "";
 
-echo "<div class='col-xl-8 offset-xl-2 py-5'>";
+echo "<div class='col-md-12'>";
 	if($action=='removed'){
 		echo "<div class='alert alert-info'>";
 			echo "Product was removed from your cart!";
@@ -59,24 +59,24 @@ if(count($_SESSION['cart'])>0){
 		$sub_total=$price*$quantity;
 
 		// =================
-		echo "<div class='row'>";
-			echo "<div class='col-md-6'>";
+		echo "<div class='cart-row'>";
+			echo "<div class='col-md-8'>";
 
-				echo "<div ><h4>{$name}</h4></div>";
+				echo "<div class='product-name m-b-10px'><h4>{$name}</h4></div>";
 
 				// update quantity
-				echo "<form class='form-group'>";
-					echo "<div class='form-control' style='display:none;'>{$id}</div>";
-					echo "<div class='form-control'>";
+				echo "<form class='update-quantity-form'>";
+					echo "<div class='product-id' style='display:none;'>{$id}</div>";
+					echo "<div class='input-group'>";
 						echo "<input type='number' name='quantity' value='{$quantity}' class='form-control cart-quantity' min='1' />";
 							echo "<span class='input-group-btn'>";
-								echo "<button class='glyphicon glyphicon-shopping-cart' type='submit'>Update</button>";
+								echo "<button class='btn btn-default update-quantity' type='submit'>Update</button>";
 							echo "</span>";
 					echo "</div>";
 				echo "</form>";
 
 				// delete from cart
-				echo "<a href='remove_from_cart.php?id={$id}' class='glyphicon glyphicon-shopping-cart'>";
+				echo "<a href='remove_from_cart.php?id={$id}' class='btn btn-default'>";
 					echo "Delete";
 				echo "</a>";
 			echo "</div>";
