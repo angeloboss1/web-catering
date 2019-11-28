2029442931348bbd004582e9142bd5ff6492e1a9
angeloboss1 <abossini@drew.edu>
2019-11-28 10:42:43 -0500

    Add too cart process

---
+++ config/database.php
@@ -0,0 +1,27 @@
+<?php
+// used to get mysql database connection
+class Database{
+
+	// specify your own database credentials
+	private $host = "localhost";
+	private $db_name = "catering_admin";
+	private $username = "catering_logins";
+	private $password = "Drew2019@";
+	public $conn;
+
+	// get the database connection
+	public function getConnection(){
+
+		$this->conn = null;
+
+		try{
+			$this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+		}catch(PDOException $exception){
+			echo "Connection error: " . $exception->getMessage();
+		}
+
+		return $this->conn;
+	}
+
+}
+?>
