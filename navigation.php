005ad0896a0befbceba19b5ad73d7cdaf8ece2e0
angeloboss1 <abossini@drew.edu>
2019-11-28 15:11:46 -0500

    Update navigation.php

--- navigation.php
+++ navigation.php
@@ -4,6 +4,7 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<nav class="navbar navbar-expand-md navbar-dark bg-dark mb-3">
 <div class="container-fluid">
 		<a href="#" class="navbar-brand mr-3">SDA Catering</a>
 		<button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
@@ -57,4 +58,5 @@
 </div>
 		</div>
 </div>
+</nav>
 <!-- /navbar -->
