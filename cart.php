a0aa8cc6eb1973608cfe9eb79e86f1d0b41f8fc7
angeloboss1 <abossini@drew.edu>
2019-11-28 15:33:48 -0500

    Update cart.php

--- cart.php
+++ cart.php
@@ -25,7 +25,11 @@ include 'layout_head.php';
 
 $action = isset($_GET['action']) ? $_GET['action'] : "";
 
-echo "<div class='col-md-12'>";
+echo "<div class="container">
+
+		<div class="row">
+
+				<div class="col-xl-8 offset-xl-2 py-5">";
 	if($action=='removed'){
 		echo "<div class='alert alert-info'>";
 			echo "Product was removed from your cart!";
