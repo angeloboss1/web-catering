3da76875ec9afd483d0ceb6f73093317c48a41fd
angeloboss1 <abossini@drew.edu>
2019-11-28 15:21:52 -0500

    Update read_products_template.php

--- read_products_template.php
+++ read_products_template.php
@@ -24,7 +24,7 @@ while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
 			}
 
 			// product name
-			echo "<img src='{$Image_path}' class='w-100-pct' />";
+			echo "<img src='{$Image_path}' width='100px' />";
 			echo "<div class='product-name m-b-10px'>{$name}</div>";
 			echo "<div class='product-name m-b-10px'>{$price}</div>";
 		echo "</a>";
