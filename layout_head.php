9b87f51137737a69ee6b8dde9762629e1f06097b
angeloboss1 <abossini@drew.edu>
2019-11-28 15:31:26 -0500

    Update layout_head.php

--- layout_head.php
+++ layout_head.php
@@ -16,13 +16,3 @@ $_SESSION['cart']=isset($_SESSION['cart']) ? $_SESSION['cart'] : array();
 <body style="height:100%;">
 
 	<?php include 'navigation.php'; ?>
-
-    <!-- container -->
-    <div class="container">
-        <div class="row">
-
-        <div class="col-md-12">
-            <div class="page-header">
-                <h1><?php echo isset($page_title) ? $page_title : "The Code of a Ninja"; ?></h1>
-            </div>
-        </div>
