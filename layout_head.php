7f545861f48e8b9b54c1a3c07f2f8f7905ffec3f
angeloboss1 <abossini@drew.edu>
2019-12-01 16:55:12 -0500

    Update layout_head.php

--- layout_head.php
+++ layout_head.php
@@ -7,11 +7,6 @@ $_SESSION['cart']=isset($_SESSION['cart']) ? $_SESSION['cart'] : array();
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <title>SDA Catering</title>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 <link href="libs/css/bootstrap/dist/css/bootstrap.css" rel="stylesheet" media="screen">
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
